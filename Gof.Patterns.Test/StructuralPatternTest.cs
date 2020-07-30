5e685829a86241b4a9bbd3825e5b14e1b26a4f02
Julio Murta <souzamurtaj@gmail.com>
2020-07-29 22:31:55 -0300

    adapter

---
+++ Gof.Patterns.Test/StructuralPatternTest.cs
@@ -0,0 +1,21 @@
+﻿using Gof.Patterns.Structural.Adapter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gof.Patterns.Test
+{
+    [TestClass]
+    public class StructuralPatternTest
+    {
+
+        [TestMethod]
+        public void Adapter_Pattern_Test()
+        {
+            var adaptee = new Adaptee();
+            var adapter = new Adapter(adaptee);
+            var expectedValue = "This is Specific request";
+
+            Assert.AreNotEqual(adaptee.GetSpecificRequest(), expectedValue);
+            Assert.AreEqual(adapter.GetRequest(), expectedValue);
+        }
+    }
+}
---
+++ Gof.Patterns/Structural/Adapter/Adapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gof.Patterns.Structural.Adapter
+{
+    public class Adapter : ITarget
+    {
+        Adaptee adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            this.adaptee = adaptee;
+        }
+
+        public string GetRequest()
+        {
+            return $"This is { this.adaptee.GetSpecificRequest() }";
+        }
+    }
+}
