d65e678971059e5d66c58422e6998c3e9a0a3d9b
Julio Murta <souzamurtaj@gmail.com>
2020-08-06 22:32:14 -0300

    chain of responsibility

---
+++ Gof.Patterns.Test/BehaviouralPatternTest.cs
@@ -0,0 +1,25 @@
+﻿using Gof.Patterns.Behavioural.ChainOfResponsibility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Gof.Patterns.Test
+{
+    [TestClass]
+    public class BehaviouralPatternTest
+    {
+        [TestMethod]
+        public void Chain_of_Responsibility_Pattern_Test()
+        {
+            var first = new FirstStep();
+            var second = new SecondStep();
+            var third = new ThirdStep();
+
+            first.Next(second).Next(third);
+
+            Assert.IsTrue((bool)first.Handler(NumberEnum.One));
+            Assert.IsTrue((bool)first.Handler(NumberEnum.Two));
+            Assert.IsTrue((bool)first.Handler(NumberEnum.Three));
+            Assert.IsNull(first.Handler(NumberEnum.Four));
+        }
+    }
+}
---
+++ Gof.Patterns/Behavioural/ChainOfResponsibility/IHandler.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gof.Patterns.Behavioural.ChainOfResponsibility
+{
+    public enum NumberEnum
+    {
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4
+    }
+
+    public interface IHandler
+    {
+        IHandler Next(IHandler handler);
+
+        object Handler(object request);
+    }
+
+    public abstract class AbstractHandler : IHandler
+    {
+        private IHandler handler;
+
+        public IHandler Next(IHandler handler)
+        {
+            this.handler = handler;
+            return this.handler;
+        }
+
+        public virtual object Handler(object request)
+        {
+            if (this.handler != null)
+            {
+                return this.handler.Handler(request);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    public class FirstStep : AbstractHandler
+    {
+        public override object Handler(object request)
+        {
+            var number = (NumberEnum)request;
+            if (number == NumberEnum.One)
+            {
+                return true;
+            }
+            else
+            {
+                return base.Handler(request);
+            }
+        }
+    }
+
+    public class SecondStep : AbstractHandler
+    {
+        public override object Handler(object request)
+        {
+            var number = (NumberEnum)request;
+            if (number == NumberEnum.Two)
+            {
+                return true;
+            }
+            else
+            {
+                return base.Handler(request);
+            }
+        }
+    }
+
+    public class ThirdStep : AbstractHandler
+    {
+        public override object Handler(object request)
+        {
+            var number = (NumberEnum)request;
+            if (number == NumberEnum.Three)
+            {
+                return true;
+            }
+            else
+            {
+                return base.Handler(request);
+            }
+        }
+    }
+}
